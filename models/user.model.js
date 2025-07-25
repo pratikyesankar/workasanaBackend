9055365f67d2713708b2f85a852552c1e9910ecc
pratik yesankar <pratikiitkgp585@gmail.com>
2025-07-25 15:32:55 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,10 @@
-  
+   
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true, maxlength: 100 },
   email: { type: String, required: true, trim: true, unique: true },
-  password: { type: String, required: true } // Stores hashed password
+  password: { type: String, required: true }  
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
